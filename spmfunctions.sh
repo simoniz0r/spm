a70217433eca536e1511eb42955fe3a322aba733
simoniz0r <bnu.fantasma@gmail.com>
2017-08-27 20:09:47 -0500

    Change version number

--- spmfunctions.sh
+++ spmfunctions.sh
@@ -6,7 +6,7 @@
 # Website: http://www.simonizor.gq
 # License: GPL v2.0 only
 
-X="0.2.0"
+X="0.2.1"
 # Set spm version
 
 helpfunc () { # All unknown arguments come to this function; display help for spm
