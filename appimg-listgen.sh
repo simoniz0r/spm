cda1272f669c847c89b5dbfe9514624ae4b60312
simoniz0r <bnu.fantasma@gmail.com>
2017-08-28 00:34:50 -0500

    Change listgen script to be less user specific

--- appimg-listgen.sh
+++ appimg-listgen.sh
@@ -2,8 +2,8 @@
 # Generate two separate lists from cloned appimagehub github repo's data folder
 
 # Generate list of github AppImages with versions that can be managed
-for image in $(dir -C -w 1 /home/simonizor/TestScripts/appimage.github.io/data); do
-    URL="$(grep -v '#' /home/simonizor/TestScripts/appimage.github.io/data/"$image" | grep '.*github*.' | cut -f-5 -d"/")"
+for image in $(dir -C -w 1 /home/$USER/github/appimage.github.io/data); do
+    URL="$(grep -v '#' /home/$USER/github/appimage.github.io/data/"$image" | grep '.*github*.' | cut -f-5 -d"/")"
     if [ ! -z "$URL" ]; then
         case $URL in
             *api.github*)
@@ -17,20 +17,20 @@ for image in $(dir -C -w 1 /home/simonizor/TestScripts/appimage.github.io/data);
         lower_image="$(echo "$image" | tr '[:upper:]' '[:lower:]')"
         echo "$lower_image $image $URL/releases https://api.github.com/repos/$REPO/releases"
     fi
-done > /home/simonizor/github/spm/AppImages-github.lst
+done > /home/$USER/github/spm/AppImages-github.lst
 # Add any AppImages that appimagehub misses
-sed -i 's%sharexin ShareXin https://github.com/ShareXin/ShareXin/releases https://api.github.com/repos/ShareXin/ShareXin/releases%%g' /home/simonizor/github/spm/AppImages-github.lst # Remove duplicate
-sed -i 's%nteract nteract https://github.com/nteract/releases https://api.github.com/repos/nteract/releases%nteract nteract https://github.com/nteract/nteract/releases https://api.github.com/repos/nteract/nteract/releases%g' /home/simonizor/github/spm/AppImages-github.lst # Fix broken link
-sed -i 's%vlc VLC https://github.com/darealshinji/releases https://api.github.com/repos/darealshinji/releases%%g' /home/simonizor/github/spm/AppImages-github.lst # Remove broken link
-sed -i '/^$/d' /home/simonizor/github/spm/AppImages-github.lst # Remove blank lines
-echo "discord discord https://github.com/simoniz0r/AppImages/releases/tag/discord https://api.github.com/repos/simoniz0r/AppImages/releases" >> /home/simonizor/github/spm/AppImages-github.lst
-echo "tc-linux tc-linux https://github.com/mccxiv/tc https://api.github.com/repos/mccxiv/tc/releases" >> /home/simonizor/github/spm/AppImages-github.lst
-echo "$(sort -u /home/simonizor/github/spm/AppImages-github.lst)" > /home/simonizor/github/spm/AppImages-github.lst
+sed -i 's%sharexin ShareXin https://github.com/ShareXin/ShareXin/releases https://api.github.com/repos/ShareXin/ShareXin/releases%%g' /home/$USER/github/spm/AppImages-github.lst # Remove duplicate
+sed -i 's%nteract nteract https://github.com/nteract/releases https://api.github.com/repos/nteract/releases%nteract nteract https://github.com/nteract/nteract/releases https://api.github.com/repos/nteract/nteract/releases%g' /home/$USER/github/spm/AppImages-github.lst # Fix broken link
+sed -i 's%vlc VLC https://github.com/darealshinji/releases https://api.github.com/repos/darealshinji/releases%%g' /home/$USER/github/spm/AppImages-github.lst # Remove broken link
+sed -i '/^$/d' /home/$USER/github/spm/AppImages-github.lst # Remove blank lines
+echo "discord discord https://github.com/simoniz0r/AppImages/releases/tag/discord https://api.github.com/repos/simoniz0r/AppImages/releases" >> /home/$USER/github/spm/AppImages-github.lst
+echo "tc-linux tc-linux https://github.com/mccxiv/tc https://api.github.com/repos/mccxiv/tc/releases" >> /home/$USER/github/spm/AppImages-github.lst
+echo "$(sort -u /home/$USER/github/spm/AppImages-github.lst)" > /home/$USER/github/spm/AppImages-github.lst
 # Generate a list of AppImages from sites other than github with versions that cannot be managed
-for image in $(dir -C -w 1 /home/simonizor/TestScripts/appimage.github.io/data); do
-    URL="$(grep -v '#' /home/simonizor/TestScripts/appimage.github.io/data/"$image" | grep -v '.*github*.')"
+for image in $(dir -C -w 1 /home/$USER/github/appimage.github.io/data); do
+    URL="$(grep -v '#' /home/$USER/github/appimage.github.io/data/"$image" | grep -v '.*github*.')"
     if [ ! -z "$URL" ]; then
         lower_image="$(echo "$image" | tr '[:upper:]' '[:lower:]')"
         echo "$lower_image $image $URL"
     fi
-done > /home/simonizor/github/spm/AppImages-direct.lst
+done > /home/$USER/github/spm/AppImages-direct.lst
