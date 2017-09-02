41a3c4aafddad18348397185a47f54767b582e44
simoniz0r <bnu.fantasma@gmail.com>
2017-09-02 14:38:19 -0500

    Add xdgfetch to listgen

--- appimg-listgen.sh
+++ appimg-listgen.sh
@@ -24,6 +24,26 @@ for image in $(dir -C -w 1 /home/$USER/github/appimage.github.io/data); do
         echo "$lower_image $image $URL/releases https://api.github.com/repos/$REPO/releases"
     fi
 done > /home/$USER/github/spm/AppImages-github.lst
+# Turn list into json
+# for image in $(dir -C -w 1 /home/$USER/github/appimage.github.io/data); do
+#     URL="$(grep -v '#' /home/$USER/github/appimage.github.io/data/"$image" | grep '.*github*.' | cut -f-5 -d"/")"
+#     if [ ! -z "$URL" ]; then
+#         case $URL in
+#             *api.github*)
+#                 REPO="$(echo $URL | cut -f5,6 -d"/")"
+#                 URL="https://github.com/$REPO"
+#                 ;;
+#             *)
+#                 REPO="$(echo $URL | cut -f4,5 -d"/")"
+#                 ;;
+#         esac
+#         lower_image="$(echo "$image" | tr '[:upper:]' '[:lower:]')"
+#         echo "$lower_image"
+#         echo "$image $URL/releases"
+#         echo "https://api.github.com/repos/$REPO/releases"
+#     fi
+# done | tr '\n' ' ' | jq -R -s 'split(" ")'
+
 # Fix/remove broken links and add any AppImages that appimagehub misses
 sed -i 's%sharexin ShareXin https://github.com/ShareXin/ShareXin/releases https://api.github.com/repos/ShareXin/ShareXin/releases%%g' /home/$USER/github/spm/AppImages-github.lst # Remove duplicate
 sed -i 's%nteract nteract https://github.com/nteract/releases https://api.github.com/repos/nteract/releases%nteract nteract https://github.com/nteract/nteract/releases https://api.github.com/repos/nteract/nteract/releases%g' /home/$USER/github/spm/AppImages-github.lst # Fix broken link
@@ -37,6 +57,7 @@ echo "neofetch neofetch https://github.com/simoniz0r/AppImages/releases https://
 echo "spm spm https://github.com/simoniz0r/spm/releases https://api.github.com/repos/simoniz0r/spm/releases" >> /home/$USER/github/spm/AppImages-github.lst
 echo "tc-linux tc-linux https://github.com/mccxiv/tc https://api.github.com/repos/mccxiv/tc/releases" >> /home/$USER/github/spm/AppImages-github.lst
 echo "vterm vterm https://github.com/vterm/vterm/releases https://api.github.com/repos/vterm/vterm/releases" >> /home/$USER/github/spm/AppImages-github.lst
+echo "xdgfetch xdgfetch https://github.com/simoniz0r/xdgfetch/releases https://api.github.com/repos/simoniz0r/xdgfetch/releases" >> /home/$USER/github/spm/AppImages-github.lst
 echo "$(sort /home/$USER/github/spm/AppImages-github.lst)" > /home/$USER/github/spm/AppImages-github.lst
 # Generate a list of AppImages from sites other than github with versions that cannot be managed
 for image in $(dir -C -w 1 /home/$USER/github/appimage.github.io/data); do
@@ -46,5 +67,16 @@ for image in $(dir -C -w 1 /home/$USER/github/appimage.github.io/data); do
         echo "$lower_image $image $URL"
     fi
 done > /home/$USER/github/spm/AppImages-direct.lst
+# Turn list into json
+# for image in $(dir -C -w 1 /home/$USER/github/appimage.github.io/data); do
+#     URL="$(grep -v '#' /home/$USER/github/appimage.github.io/data/"$image" | grep -v '.*github*.')"
+#     if [ ! -z "$URL" ]; then
+#         lower_image="$(echo "$image" | tr '[:upper:]' '[:lower:]')"
+#         echo "$lower_image"
+#         echo "$image"
+#         echo "$URL"
+#     fi
+# done | tr '\n' ' ' | jq -R -s 'split(" ")'
+
 sed -i 's%wastesedge wastesedge http://download.savannah.gnu.org/releases/adonthell/wastesedge-0.3.6-x86_64-linux.tar.gz%%g' /home/$USER/github/spm/AppImages-direct.lst # Remove this; not going to support AppImages in tar archives
 sed -i '/^$/d' /home/$USER/github/spm/AppImages-direct.lst # Remove blank lines
