1a497777586d224e1e9d6a96122c6a99f2e0a841
simoniz0r <bnu.fantasma@gmail.com>
2017-09-20 18:53:36 -0500

    Change listgen to output inividual files for spm-repo

--- appimg-listgen.sh
+++ appimg-listgen.sh
@@ -6,7 +6,7 @@
 # Website: http://www.simonizor.gq
 # License: GPL v2.0 only
 # Generate two separate lists from cloned appimagehub github repo's data folder
-
+# MODIFY THIS TO OUTPUT TO INDIVIDUAL YAML FILES FOR SPM-REPO
 # Generate list of github AppImages with versions that can be managed
 for image in $(dir -C -w 1 /home/$USER/github/spm-repo/data); do
     URL="$(grep -v '#' /home/$USER/github/spm-repo/data/"$image" | grep '.*github*.' | cut -f-5 -d"/")"
@@ -20,13 +20,19 @@ for image in $(dir -C -w 1 /home/$USER/github/spm-repo/data); do
                 REPO="$(echo $URL | cut -f4,5 -d"/")"
                 ;;
         esac
-        lower_image="$(echo "$image" | tr '[:upper:]' '[:lower:]')"
-        echo "$lower_image:"
-        echo "    name: $image"
-        echo "    url: $URL/releases"
-        echo "    apiurl: https://api.github.com/repos/$REPO/releases"
+        lower_image="$(echo "$image" | tr '[:upper:]' '[:lower:]' | tr -d '.')"
+        # echo "$lower_image:"
+        if [ ! -f "/home/$USER/github/spm-repo/AppImages-github/$lower_image.yml" ]; then
+            echo "name: $image" > /home/$USER/github/spm-repo/AppImages-github/"$lower_image".yml
+            echo "url: $URL/releases" >> /home/$USER/github/spm-repo/AppImages-github/"$lower_image".yml
+            echo "apiurl: https://api.github.com/repos/$REPO/releases" >> /home/$USER/github/spm-repo/AppImages-github/"$lower_image".yml
+            echo "description: $(wget --quiet "$URL" -O - | grep -i '<meta name="description"' | cut -f4 -d'"' | sed "s/[^a-zA-Z']/ /g")" >> /home/$USER/github/spm-repo/AppImages-github/"$lower_image".yml
+            echo "$(tput setaf 6)yaml file for $image has been generated!$(tput sgr0)"
+        else
+            echo "$(tput setaf 5)yaml file for $image already exists; skipping...$(tput sgr0)"
+        fi
     fi
-done > /home/$USER/github/spm/AppImages-github.yaml
+done # > /home/$USER/github/spm/AppImages-github.yaml
 
 
 # Generate a list of AppImages from sites other than github with versions that cannot be managed
@@ -34,9 +40,15 @@ for image in $(dir -C -w 1 /home/$USER/github/spm-repo/data); do
     URL="$(grep -v '#' /home/$USER/github/spm-repo/data/"$image" | grep -v '.*github*.')"
     if [ ! -z "$URL" ]; then
         lower_image="$(echo "$image" | tr '[:upper:]' '[:lower:]')"
-        echo "$lower_image:"
-        echo "    name: $image"
-        echo "    url: $URL"
+        # echo "$lower_image:"
+        echo "name: $image" > /home/$USER/github/spm-repo/AppImages-other/"$lower_image".yml
+        echo "url: $URL" >> /home/$USER/github/spm-repo/AppImages-other/"$lower_image".yml
+        if [ ! -f "/home/$USER/github/spm-repo/AppImages-other/$lower_image.yml" ]; then
+            echo "$(tput setaf 6)yaml file for $image has been generated!$(tput sgr0)"
+        else
+            echo "$(tput setaf 5)yaml file for $image already exists.$(tput sgr0)"
+        fi
     fi
-done > /home/$USER/github/spm/AppImages-direct.yaml
+
+done # > /home/$USER/github/spm/AppImages-direct.yaml
 
