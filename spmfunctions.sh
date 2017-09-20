46f3216d796f6b065efe6760795cc7453ebc1610
simoniz0r <bnu.fantasma@gmail.com>
2017-09-19 20:40:42 -0500

    Fix add tar.xz support

--- spmfunctions.sh
+++ spmfunctions.sh
@@ -6,7 +6,7 @@
 # Website: http://www.simonizor.gq
 # License: GPL v2.0 only
 
-X="0.4.5"
+X="0.4.6"
 # Set spm version
 
 helpfunc () { # All unknown arguments come to this function; display help for spm
--- tarfunctions.sh
+++ tarfunctions.sh
@@ -6,7 +6,7 @@
 # Website: http://www.simonizor.gq
 # License: GPL v2.0 only
 
-X="0.4.5"
+X="0.4.6"
 # Set spm version
 TAR_LIST="$(echo -e $(grep '"available"' "$CONFDIR"/tar-pkgs.json | cut -f7 -d" " | tr -d ',"'))"
 TAR_SIZE="N/A"
