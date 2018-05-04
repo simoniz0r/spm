991a7f3e9e4390853078cc5fb1c61545cda58a1b
simoniz0r <bnu.fantasma@gmail.com>
2018-05-04 16:57:15 -0500

    Add completion scripts

---
+++ bash-completion.sh
@@ -0,0 +1,35 @@
+__spm2() {
+    local cur prev opts base
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    #
+    #  The basic options we'll complete.
+    #
+    opts="list info install get remove update revert freeze config man help --verbose --debug"
+
+
+    #
+    #  Complete the arguments to some of the basic commands.
+    #
+    case "${prev}" in
+        install|in|info|i|get)
+            local packagelist="$(dir -C -w 1 ~/.local/share/spm/list | sed 's%.json%%g')"
+            COMPREPLY=( $(compgen -W "${packagelist}" -- ${cur}) )
+            return 0
+            ;;
+        remove|rm|update|up|revert|rev|freeze|fr)
+            local packagesinstalled="$(dir -C -w 1 ~/.local/share/spm/installed)"
+            COMPREPLY=( $(compgen -W "${packagesinstalled}" -- ${cur}) )
+            return 0
+            ;;
+        *)
+        ;;
+    esac
+
+   COMPREPLY=($(compgen -W "${opts}" -- ${cur}))  
+   return 0
+}
+
+complete -F __spm2 spm2
---
+++ zsh-completion.sh
@@ -0,0 +1,27 @@
+#compdef spm2
+
+_spm2() {
+    local curcontext="$curcontext" state line
+    typeset -A opt_args
+ 
+    _arguments \
+        '1: :->args'\
+        '2: :->input'
+ 
+    case $state in
+    args)
+        _arguments '1:arguments:(list info search install get remove update revert freeze config man help --verbose --debug)'
+        ;;
+    *)
+        case $words[2] in
+        install|in|info|i|get)
+            compadd "$@" $(dir -C -w 1 ~/.local/share/spm/list | sed 's%.json%%g')
+            ;;
+        remove|rm|update|up|revert|rev|freeze|fr)
+            compadd "$@" $(dir -C -w 1 ~/.local/share/spm/installed)
+            ;;
+        esac
+    esac
+}
+
+_spm2
